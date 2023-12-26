30d13737abbd1aca624d207c8137666ff6ebe512
m8kyself <19498877+m8kyself@users.noreply.github.com>
2023-12-26 02:31:25 +0000

    Adapt trees to arrow (dlearin)

--- arrow_redwood.mk
+++ arrow_redwood.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from redwood device
 $(call inherit-product, device/xiaomi/redwood/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 PRODUCT_BRAND := POCO
 PRODUCT_DEVICE := redwood
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 22101320G
-PRODUCT_NAME := lineage_redwood
+PRODUCT_NAME := arrow_redwood
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
